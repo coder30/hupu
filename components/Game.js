0b3d1f926a509eefc2282387280c0805a3f04c11
curry <1186632641@qq.com>
2019-02-28 14:49:42 +0800

    增加了跳转到今日比赛的悬浮按钮

--- components/Game.js
+++ components/Game.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import { Font } from 'expo';
+import MD5  from"react-native-md5";
 import { logo, name } from '../constants/Team';
 import { View, Text, FlatList, Image, ActivityIndicator, StyleSheet, TouchableWithoutFeedback} from 'react-native';
-
 export default class Game extends React.Component {
     constructor(props){
         super(props);
         this.state = {isLoading: true, index: 0}
     }
-    componentDidMount() {
-        Font.loadAsync({
-          'DINCond-Bold': require('../assets/fonts/DINCond-Bold.otf'),
-        });
-        return fetch('https://games.mobileapi.hupu.com/1/7.3.2/nba/getMatchs?crt=1548734093560&night=0&channel=miui&sign=32cd5aaf9265ab0333397d618529f7a7&client=316810195181635&_ssid=IkExMzAzLTVHIg==&time_zone=Asia/Shanghai&android_id=36f92b5c1454e1e9')
+    loadData(){
+      let time = new Date().getTime();
+      let res = "_ssid=IkExMzAzLTVHIg==&android_id=36f92b5c1454e1e9&channel=miui&client=316810195181635&crt="+time+"&night=0&time_zone=Asia/ShanghaiHUPU_SALT_AKJfoiwer394Jeiow4u309"
+      let sign = MD5.hex_md5(res);
+      let url = "https://games.mobileapi.hupu.com/1/7.3.2/nba/getMatchs?crt="+time+"&night=0&channel=miui&sign="+sign+"&client=316810195181635&_ssid=IkExMzAzLTVHIg==&time_zone=Asia/Shanghai&android_id=36f92b5c1454e1e9"
+      console.log(url);
+      fetch(url)
           .then((Response)=>Response.json())
           .then((ResponseJson) => {
             var res = ResponseJson.result.games;
@@ -50,7 +52,22 @@ export default class Game extends React.Component {
           .catch((error)=>{
             console.log(error);
           })
-      }
+    }
+    componentWillUnMount (){
+      clearInterval(this.intervalId);
+    }
+    componentDidMount() {
+        Font.loadAsync({
+          'DINCond-Bold': require('../assets/fonts/DINCond-Bold.otf'),
+        });
+        this.loadData();
+        this.intervalId = setInterval(() => {
+          this.loadData();
+        }, 5000);
+    }
+    goIndex = () => {
+      this.flatListRef.scrollToIndex({animated: true, index: this.state.index});
+    };
     render() {
         if(this.state.isLoading){
             return(
@@ -60,10 +77,18 @@ export default class Game extends React.Component {
             )
         }
         return (
+          <View>
+            <TouchableWithoutFeedback onPress={()=>{
+                this.goIndex()
+              }
+            }>
+              <Image source={require('../assets/images/today.png')} style={{zIndex: 2,position: 'absolute' ,bottom: 60, right:10 ,width: 80, height: 80}}/>
+            </TouchableWithoutFeedback>
             <FlatList
                 keyExtractor={(item, index) => 'key'+index}
-                ref={el => this.list = el}
+                ref={(ref) => { this.flatListRef = ref; }}
                 extraData={this.state}
+                style={{zIndex: 1, marginBottom: 50}}
                 data={this.state.dataSource} 
                 showsVerticalScrollIndicator = {false}
                 initialNumToRender={10}
@@ -72,6 +97,7 @@ export default class Game extends React.Component {
                     <Text style={styles.gameDate}>{item.date_block}</Text>
                     <FlatList
                         data={item.data}
+                        extraData={this.state}
                         keyExtractor={(item, index) => 'key'+index}
                         getItemLayout={(data, index) => (
                           // 120 是被渲染 item 的高度 ITEM_HEIGHT。
@@ -105,29 +131,35 @@ export default class Game extends React.Component {
                                   :<View style={{marginRight: 16}}></View>
                                   }
                                   <View >
-                                  {item.status.id==2
-                                      ?<Text style={{color: "#C01E2F" ,textAlign:'center', fontWeight: 'bold', fontSize: 15.5}}>LIVE</Text>
-                                      :null
-                                  }
-                                  {item.status.id==3
-                                      ?<View style={{flexDirection: 'row', height: 18, marginBottom: 2}}>
-                                      <Text style={styles.live}>LIVE</Text>
-                                      <Text style={styles.gameTime}>{item.status.txt.slice(0,item.status.txt.indexOf('开'))}</Text>
-                                      </View>
-                                      :null
-                                  }
-                                  <Text style={{color: 'rgba(0, 0, 0, 0.38)' ,textAlign:'center', fontSize: 12.5, marginBottom:3}}>{item.title}</Text>
-                                  {item.status.id==2
-                                      ?<View style={{flexDirection: 'row', height: 18, marginBottom: 15, justifyContent: 'center'}}>
-                                      <Text style={styles.part}>{item.status.txt.slice(0,item.status.txt.indexOf(' '))}</Text>
-                                      <Text style={styles.gameTime}>{item.status.txt.slice(item.status.txt.indexOf(' '))}</Text>
-                                      </View>
-                                      :null
-                                  }
-                                  {item.status.id==1
-                                      ?<Text style={{textAlign:'center', fontSize: 12, color: 'rgba(0, 0, 0, 0.38)'}}>结束</Text>
-                                      :null
-                                  }
+                                    {item.status.id==2
+                                        ?<Text style={{color: "#C01E2F" ,textAlign:'center', fontWeight: 'bold', fontSize: 15.5}}>LIVE</Text>
+                                        :null
+                                    }
+                                    {item.status.id==3
+                                        ?<View style={{flexDirection: 'row', height: 18, marginBottom: 2}}>
+                                        <Text style={styles.live}>LIVE</Text>
+                                        <Text style={styles.gameTime}>{item.status.txt.slice(0,item.status.txt.indexOf('开'))}</Text>
+                                        </View>
+                                        :null
+                                    }
+                                    <Text style={{color: 'rgba(0, 0, 0, 0.38)' ,textAlign:'center', fontSize: 12.5, marginBottom:3}}>{item.title}</Text>
+                                    {item.status.id==2 && item.status.txt!='即将开始'
+                                        ?<View style={{flexDirection: 'row', height: 18, marginBottom: 15, justifyContent: 'center'}}>
+                                          <Text style={styles.part}>{item.status.txt.slice(0,item.status.txt.indexOf(' '))}</Text>
+                                          <Text style={styles.gameTime}>{item.status.txt.slice(item.status.txt.indexOf(' '))}</Text>
+                                        </View>
+                                        :null
+                                    }
+                                    {item.status.id==2 && item.status.txt=='即将开始'
+                                        ?<View style={{flexDirection: 'row', height: 18, marginBottom: 15, justifyContent: 'center'}}>
+                                          <Text style={styles.part}>{item.status.txt}</Text>
+                                        </View>
+                                        :null
+                                    }
+                                    {item.status.id==1
+                                        ?<Text style={{textAlign:'center', fontSize: 12, color: 'rgba(0, 0, 0, 0.38)'}}>结束</Text>
+                                        :null
+                                    }
                                   </View>
                                   {item.away_score!="" ?
                                   <Text style={{padding:5, fontFamily: 'DINCond-Bold', fontSize: 24, color:item.away_color, minWidth:40}}>{item.away_score}</Text>
@@ -149,6 +181,7 @@ export default class Game extends React.Component {
                     }/>
                 </View>
             }/>
+            </View>
         );
     }
 }
